a7af1e7c89ac45b20ae9035dbf069c1330c74d6c
VaibhaV246 <103430588+VaibhaV246@users.noreply.github.com>
2022-04-11 19:25:35 +0530

    Add files via upload

---
+++ Basics/CompInterpret.java
@@ -0,0 +1,18 @@
+class CompInterpret
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Java output 1");
+
+		System.out.println("Java output 2");
+
+		int n= 0/0; 
+
+		System.out.println("Java output 3");
+
+		int m= 2/2; 
+
+		System.out.println("Java output 4");
+	}
+
+}
